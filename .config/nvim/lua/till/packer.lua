52a097b1cb1e9a37e579d266f5f0c85620938768
Till Kämmerer <dev@till.kaemmerer.me>
2025-02-07 15:39:56 +0100

    update

--- .config/nvim/lua/till/packer.lua
+++ .config/nvim/lua/till/packer.lua
@@ -79,4 +79,22 @@ return require('packer').startup(function(use)
       "nvim-treesitter/nvim-treesitter"
     }
   }
+
+  use {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = function()
+      require("nvim-autopairs").setup {}
+    end
+  }
+
+  use({
+    "kylechui/nvim-surround",
+    tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end
+  })
 end)
