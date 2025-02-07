8707359bd4068dc0841856f5be0f258363da5236
Till Kämmerer <till.kaemmerer@valoon.chat>
2025-02-07 15:31:56 +0100

    update

--- .config/fish/functions/config.fish
+++ .config/fish/functions/config.fish
@@ -1,1 +1,4 @@
-alias config '/usr/bin/git --git-dir=/Users/tillkammerer/.cfg/ --work-tree=/Users/tillkammerer'
+function config --wraps='/usr/bin/git --git-dir=/Users/tillkammerer/.cfg/ --work-tree=/Users/tillkammerer' --wraps='git --git-dir=$HOME/.cfg/ --work-tree=$HOME' --description 'alias config=git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+  git --git-dir=$HOME/.cfg/ --work-tree=$HOME $argv
+        
+end
